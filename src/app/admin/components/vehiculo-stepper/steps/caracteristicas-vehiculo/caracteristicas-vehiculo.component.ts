4257d2af9075f3cee4f73d060b812dcf467455ad
Alan Bersia <alanbersia@gmail.com>
2025-08-13 13:44:58 -0300

    Ultimos cambios

--- src/app/admin/components/vehiculo-stepper/steps/caracteristicas-vehiculo/caracteristicas-vehiculo.component.ts
+++ src/app/admin/components/vehiculo-stepper/steps/caracteristicas-vehiculo/caracteristicas-vehiculo.component.ts
@@ -6,4 +6,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './caracteristicas-vehiculo.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CaracteristicasVehiculoComponent { }
+export class CaracteristicasVehiculoComponent {
+  
+
+  private getMainFeatures() {
+    
+  }
+}
